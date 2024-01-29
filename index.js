ad80464f89a7bdf39e4581eebfe1dba22247bafd
callmeniyu <getniyashere@gmail.com>
2024-01-29 21:26:24 +0530

    Final commit

--- index.js
+++ index.js
@@ -23,40 +23,44 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-app.get("/", async (req, resp) => {
-    db.query("SELECT country_code FROM visited_countries", (err, res) => {
-        if (err) console.error("Failed to retrieve data", err.stack)
-        else {
-            res.rows.forEach((code) => {
-                if(visitedCountry_codes.includes(code.country_code)) return
-                visitedCountry_codes.push(code.country_code)
+async function checkVisisted() {
+    const result = await db.query("SELECT country_code FROM visited_countries");
+    let countries = [];
+    result.rows.forEach((country) => {
+        countries.push(country.country_code);
+    });
+}
+
+app.get("/", async (req, res) => {
+    const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+})
+
+app.post("/add", async (req, res) => {
+    const requestedCountry = req.body.country
+    try {
+        const result = await db.query("SELECT country_code FROM countries  WHERE country_name LIKE '%' || $1 || '%' ", [
+            requestedCountry.toLowerCase(),
+        ])
+        const data = result.rows[0]
+        const countryCode = data.country_code
+        try {
+            await db.query(`INSERT INTO visited_countries (country_code) VALUES($1)`, [countryCode])
+        } catch (err) {
+            res.render("index", {
+                countries: visitedCountry_codes,
+                total: visitedCountry_codes.length,
+                error: "Given country already added",
             })
         }
-        resp.render("index", {
+    } catch (err) {
+        res.render("index", {
             countries: visitedCountry_codes,
             total: visitedCountry_codes.length,
+            error: "Given country doesn't exist",
         })
-        console.log(visitedCountry_codes);
-    })
+    }
 })
-
-app.post("/add", (req, resp) => {
-    const requestedCountry = req.body.country
-    db.query(`SELECT country_code FROM countries  WHERE country_name = '${requestedCountry}'`, (err, res) => {
-        if (err) console.error("Error retrieving data from Countries", err.stack)
-        else {
-            const country_code = res.rows[0].country_code
-            db.query(`INSERT INTO visited_countries (country_code) VALUES($1)`, [`${country_code}`], (err, res) => {
-                if (err) console.error("Error inserting data to Visited", err.stack)
-                else {
-                    console.log(requestedCountry +" Country Succesfully Added")
-                    resp.redirect("/")
-                }
-            })
-        }
-    })
-})
-
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
 })
--- solution4.js
+++ solution4.js
@@ -3,13 +3,14 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+
+const port = 4000;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: "postgre987",
   port: 5432,
 });
 db.connect();
